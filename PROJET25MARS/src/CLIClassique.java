6c012c56baef7089bea64da8062cef4036cdc535
elya01 <44399210+elya01@users.noreply.github.com>
2023-03-02 14:59:42 +0100

    rajout des exception

--- PROJET25MARS/src/CLIClassique.java
+++ PROJET25MARS/src/CLIClassique.java
@@ -5,17 +5,29 @@ public class CLIClassique {
 		boolean finOptions = false;
 		int i = 0;
 		while (i < args.length && ! finOptions) {
-			String arg = args[i];
-			switch (arg) {
-				case "-K": // Valeur de l'indice à calculer
-					config.indice = Integer.parseInt(args[++i]);
-					break;
+
+            String arg = args[i];
+
+            switch (arg) {
+
+                    case "-K": // Valeur de l'indice à calculer
+                        try {
+                        config.indice = Integer.parseInt(args[++i]);
+                        break;
+                }catch (NumberFormatException e) {
+                    System.out.println("la valeur n'est pas valide veuillez entrer un nombre" +args[i]);}
 				case "-E": // Valeur de la précision à atteindre
+                    try{
 					config.epsilon = Double.parseDouble(args[++i]);
-					break;
+					break;}catch (NumberFormatException e){
+                        System.out.println("le format n'est pas valide, veuillez entrer un nombre");
+                    }
 				case "-A": // Valeur de alpha
+                    try{
 					config.alpha = Double.parseDouble(args[++i]);
-					break;
+					break;}catch (NumberFormatException e){
+                        System.out.println("le format n'est pas valide, veuillez entrer un nombre ");
+                }
 				case "-C": // Mode matrice creuse
 					config.mode = Mode.CREUSE;
 					break;
